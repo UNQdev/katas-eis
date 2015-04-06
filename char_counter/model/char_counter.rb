4e36820a3590dd37fccddde808a08aa107fefb44
bananee <rubini.marcelo@gmail.com>
2015-04-06 06:15:32 +0000

    Requerimiento: Retorna {} cuando el string es nil

--- char_counter/model/char_counter.rb
+++ char_counter/model/char_counter.rb
@@ -1,15 +1,20 @@
 class CharCounter
 
     def self.count(string)
-       counted = Hash.new 0
-       
-       string.each_char do |char|
-           counted[char] += 1
-       end
-       
-       counted.default = nil
-       
-       counted
+        
+        if string.equal? nil
+            {}
+        else
+            counted = Hash.new 0
+            
+            string.each_char do |char|
+                counted[char] += 1
+            end
+            
+            counted.default = nil
+            counted
+        end
+        
     end
     
 end
