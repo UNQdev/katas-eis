43a1e8d62ef36bae3db72f41fc7e35f3f644422f
Marcelo Rubini <rubini.marcelo@gmail.com>
2015-05-06 17:32:37 -0300

    Error en el auto-merge corregido

--- batalla_naval/features/step_definitions/battleship_steps.rb
+++ batalla_naval/features/step_definitions/battleship_steps.rb
@@ -1,15 +1,10 @@
 require_relative '../../app/models/battleship.rb'
-<<<<<<< HEAD
-=======
-require_relative '../../app/models/ship.rb'
->>>>>>> 6182a1b2eb912781e115d649ed03494b7e97959c
 
 Given(/^a battleship board with dimensions "(.*?)" x "(.*?)"$/) do |dimension, arg2|
   @battleship = Battleship.new dimension
 end
 
 When(/^I create a small ship in position "(.*?)":"(.*?)"$/) do |row,col|
-<<<<<<< HEAD
 	position = [[row,col]]
 	@battleship.place_ship(Ship.new(position))
 end
@@ -32,19 +27,5 @@ When(/^I create a small ship in invalid position "(.*?)":"(.*?)"$/) do |row,col|
 end
 
 Then(/^it should raise a "(.*?)" error$/) do |error_msg|
-  expect { @battleship.place_ship(@ship) }.to raise_error(error_msg)
-=======
-	positions = [[row,col]]
-	@battleship.place_ship(Ship.new(positions))
+	expect { @battleship.place_ship(@ship) }.to raise_error(error_msg)
 end
-
-Then(/^position "(.*?)":"(.*?)" is not empty$/) do |row,col|
-	@battleship.ship_placed?([[row,col]])
->>>>>>> 6182a1b2eb912781e115d649ed03494b7e97959c
-end
-
-Then(/^position "(.*?)":"(.*?)" should not be valid$/) do |row,col|
-	positions = [[row,col]]
-	expect(@battleship.valid_positions?(Ship.new(positions))).to eq false
-end
-
