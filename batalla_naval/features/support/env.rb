c554a76b6103ee9c4d9022c54b5882f0f89b5e6c
Marcelo Rubini <rubini.marcelo@gmail.com>
2015-05-29 13:13:13 -0300

    fix require_relative

--- batalla_naval/features/support/env.rb
+++ batalla_naval/features/support/env.rb
@@ -1,8 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + "/../../config/boot")
-require_relative '../../app/app'
-
 require 'capybara/cucumber'
 require 'rspec/expectations'
+require_relative '../../app/app'
 
 # Capybara.default_driver = :selenium
 Capybara.app = BattleshipApp::App.tap { |app|  }
